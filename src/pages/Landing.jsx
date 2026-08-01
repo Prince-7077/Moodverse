3a1c7c14c67b6deb460334959a53619957fe7f85
PRINCE KUMAR <prince25b@iiitg.ac.in>
2026-08-02 01:26:39 +0530

    Rename Components folder to lowercase

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,10 +2,10 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import MoodSection from "../components/MoodSection";
 import Features from "../components/Features";
-import CategorySection from "../Components/CategorySection";
+import CategorySection from "../components/CategorySection";
 import FeaturesSection from "../components/FeaturesSection";
 import Recommendations from "./Recommendations";
-import Footer from "../Components/Footer";
+import Footer from "../components/Footer";
 
 const Landing = () => {
   return (
